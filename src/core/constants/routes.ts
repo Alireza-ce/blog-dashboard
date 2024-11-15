8fcd6d271514126cebaa0a37bafd1f510b6f4b67
alireza rezaei <rezaei.alireza.ce@gmail.com>
2024-11-15 15:11:15 +0330

    fix: add route guard

---
+++ src/core/constants/routes.ts
@@ -0,0 +1,1 @@
+export const notProtectedRoutes = ["Login", "Register"];
--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,8 @@ import Login from "./modules/auth/views/Login.vue";
 import Register from "./modules/auth/views/Register.vue";
 import Articles from "./modules/dashboard/veiws/Articles.vue";
 import Dashboard from "./modules/dashboard/veiws/Dashboard.vue";
+import { isLoggedIn } from "./core/services/authentication";
+import { notProtectedRoutes } from "./core/constants/routes";
 
 const routes = [
   {
@@ -30,6 +32,16 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const isProtectedRoute = !notProtectedRoutes.includes(
+    to.name?.toString() || "",
+  );
+  const userIsAuthenticated = isLoggedIn();
+
+  if (isProtectedRoute && !userIsAuthenticated) next({ name: "Login" });
+  else next();
 });
 
 export default router;
